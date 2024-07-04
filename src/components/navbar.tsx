19b745bd6bb7387c3f46701b9745228d11fe2cf5
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-07-04 13:36:47 -0600

    fix: Made the navbar better on small screens

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,15 +3,16 @@ import { ModeToggle } from "./mode-toggle";
 
 const NavBar = () => {
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center sticky top-0  backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 dark:border-gray-700 z-40">
+    <header className="px-4 lg:px-6 h-14 flex items-center justify-between sticky top-0 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 dark:border-gray-700 z-40">
       <Link
         href="/"
-        className="flex items-center justify-center block sm:hidden"
+        className="flex items-center justify-center"
         prefetch={false}
       >
-        <h1 className="font-glitch-font text-[50px]">THUNDERHACK</h1>
+        <h1 className="block md:hidden font-glitch-font text-xl">THUNDERHACK</h1> {/* Smaller text or logo for mobile */}
+        <h1 className="hidden md:block font-glitch-font text-[50px]">THUNDERHACK</h1>
       </Link>
-      <nav className="ml-auto gap-4 sm:gap-6 text-center flex justify-center items-center">
+      <nav className="ml-auto gap-2 sm:gap-6 text-center flex justify-center items-center">
         <Link
           href="#features"
           className="text-sm font-medium hover:underline underline-offset-4"
