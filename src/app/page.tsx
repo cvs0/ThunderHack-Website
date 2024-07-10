d007eff4fb64aa6e8d04fa2eb3ef6859949f8835
06ED <hsbest14@yandex.ru>
2024-07-11 01:05:31 +0300

    link to docs page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
                     Download Now
                   </Link>
                   <Link
-                    href="#"
+                    href="/docs"
                     className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
