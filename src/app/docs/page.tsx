457007b3b270ad1dddd2f91cb67420576c3b2b15
06ED <hsbest14@yandex.ru>
2024-07-11 01:04:50 +0300

    coming soon docs page

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,15 @@
+const Docs = () => {
+    return (
+        <div className="flex flex-col min-h-[100dvh]">
+            <main className="flex-1">
+                <div className="py-24">
+                    <h1 className="text-center text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                        Coming <span className="bg-gradient-to-r from-purple-500 to-sky-500 text-transparent bg-clip-text">soon</span>
+                    </h1>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default Docs;
